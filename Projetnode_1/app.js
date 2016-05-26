0e9dc45ae2b5fca8cfd19fb0e73b71ae1565356d
raph63 <raphael.christian-roy@polymtl.ca>
2016-05-26 14:59:47 -0400

    Add http errors handlers

--- Projetnode_1/app.js
+++ Projetnode_1/app.js
@@ -2,13 +2,20 @@
 
 // solution : use Node.js to connect to treehouse's API to get profile information to print out
 var https = require("https");
+var http = require("http");
 var username = "francismarineau";
 
+
+// Print out message
 function printMessage(username, badgeCount, points) {
 	var message = username + " has " + badgeCount + " total badges and " + points + " Points in JS";
 	console.log(message);
 }
 
+// Print out error mess
+function printError(error) {
+	console.error(error.message);
+}
 
 //connect to API URL (http://teamtreehouse.com/username.json)
 var request = https.get("https://teamtreehouse.com/" + username + ".json", function(res) {
@@ -18,12 +25,20 @@ var request = https.get("https://teamtreehouse.com/" + username + ".json", funct
   });
 
   res.on('end', () => {
-    var profile = JSON.parse(body);
-    printMessage(username, profile.badges.length, profile.points.JavaScripts);
+  	if(res.statusCode === 200) {
+	    try {
+	    	var profile = JSON.parse(body);
+	    	printMessage(username, profile.badges.length, profile.points.JavaScripts);
+	  	} catch(error) {
+		  	// Parse error 
+		  	printError(error);
+		  	}
+	  } else {
+	  	printError({ message: "There was an error getting the profile for " + username + ". (" + http.STATUS_CODES[res.statusCode] + ")" })
+	  }
   });
 });
 
-request.on("error", function(error) {
-  console.error(error.message);
-});
+// Connection error
+request.on("error", printError);
 
